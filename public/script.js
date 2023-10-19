9ebdbf5b07d9e9a37f6cf9da30db812443c8565a
Kristiana Ristani <k_ristani@yahoo.com>
2023-10-19 18:05:15 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,71 +1,103 @@
-const tmdbKey = '30c84533e38f0a2b30bfb14241a38016';
+const tmdbKey = "30c84533e38f0a2b30bfb14241a38016";
 const tmdbBaseUrl = "https://api.themoviedb.org/3";
 const playBtn = document.getElementById("playBtn");
 
-const getGenres = async () => {
-const genreRequestEndpoint = '/genre/movie/list';
-const requestParams = `?api_key=${tmdbKey}`;
-const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
-//console.log(urlToFetch)
-try {  
-  const response = await fetch(urlToFetch);
-  if(response.ok){
-    const jsonResponse = await response.json();
-   // console.log(jsonResponse)
-    const genres = jsonResponse.genres;
-    return genres;
+const getGenres = async (movie) => {
+  const genreRequestEndpoint = "/genre/movie/list";
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
     }
-} catch (error){
-  console.log(error);
-};
+  } catch (error) {
+    console.log(error);
+  };
 };
 
-const getMovies = async () => {
+const getMovies = async() => {
   const selectedGenre = getSelectedGenre();
-  const discoverMovieEndpoint = '/discover/movie';
+  const discoverMovieEndpoint = "/discover/movie";
   const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
   const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
-  try { 
+
+  try{
     const response = await fetch(urlToFetch);
-    if(response.ok){
+    if (response.ok) {
       const jsonResponse = await response.json();
       const movies = jsonResponse.results;
+      
       return movies;
-  }
 
-  } catch (error) {
-    console.log(error)
-  }
-};
+  }}
+  catch(error){
+    console.log(error);
+ 
+}};
 
 
-const getMovieInfo = async (movie) => {
+const getSimilarMovies = async(movie) => {
+  const movieId = movie.id;
+  const movieEndpoint = `/movie/${movieId}/similar`;
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
+
+  try{
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results;
+      console.log(movies);
+      return movies;
+
+  }}
+  catch(error){
+    console.log(error);
+ 
+}};
+
+
+
+
+const getMovieInfo = async(movie) => {
   const movieId = movie.id;
   const movieEndpoint = `/movie/${movieId}`;
   const requestParams = `?api_key=${tmdbKey}`;
   const urlToFetch = `${tmdbBaseUrl}${movieEndpoint}${requestParams}`;
-  try{ 
+
+  try{
     const response = await fetch(urlToFetch);
-    if (response.ok){
-      const movieInfo = await response.json()
-     return movieInfo;
-     }
-  } catch (error){
+    if (response.ok) {
+      const movieInfo = await response.json();
+      return movieInfo;
+
+  }}
+  catch(error){
     console.log(error);
-  }
-  };
+ 
+};
+getSimilarMovies(movie);
+
+};
 
 // Gets a list of movies and ultimately displays the info of a random movie from the list
-const showRandomMovie = async () => {
-  const movieInfo = document.getElementById('movieInfo');
+const showRandomMovie = async() => {
+  const movieInfo = document.getElementById("movieInfo");
   if (movieInfo.childNodes.length > 0) {
     clearCurrentMovie();
-  };
-const movies = await getMovies();
-const randomMovie = getRandomMovie(movies);
-const info = await getMovieInfo(randomMovie);
-displayMovie(info)
+  }
+  const movies = await getMovies();
+  const randomMovie = getRandomMovie(movies);
+  const info = await getMovieInfo(randomMovie);
+  
+  const similarMovies = await getSimilarMovies(randomMovie);
+  await displayMovie(info, similarMovies);
 };
 
 getGenres().then(populateGenreDropdown);
 playBtn.onclick = showRandomMovie;
+showRandomMovie();
