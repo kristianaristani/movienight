e15ee9e87808d29e28179e3c33cf821a5a7331f8
Kristiana Ristani <k_ristani@yahoo.com>
2023-10-19 18:14:19 +0200

    Update helpers.js

--- public/helpers.js
+++ public/helpers.js
@@ -79,7 +79,7 @@ const getRandomMovie = (movies) => {
 };
 
 // Uses the DOM to create HTML to display the movie
-const displayMovie = (movieInfo) => {
+const displayMovie = (movieInfo,similarMovies) => {
     const moviePosterDiv = document.getElementById('moviePoster');
     const movieTextDiv = document.getElementById('movieText');
     const likeBtn = document.getElementById('likeBtn');
@@ -96,6 +96,43 @@ const displayMovie = (movieInfo) => {
     movieTextDiv.appendChild(overviewText);
   
     showBtns();
+    displaySimilarMovies(similarMovies);
     likeBtn.onclick = likeMovie;
     dislikeBtn.onclick = dislikeMovie;
+};
+
+const displaySimilarMovies = (similarMovies) => {
+  // Assuming that you have a div element to display similar movies with the id "similarMovies"
+  const similarMoviesDiv = document.getElementById('similarMovies');
+  
+  // Clear any previous similar movie content
+  similarMoviesDiv.innerHTML = '';
+
+  if (similarMovies && similarMovies.length > 0) {
+    const numToShow = 3; // You can change this number as needed
+
+    for (let i = 0; i < numToShow && i < similarMovies.length; i++) {
+      const similarMovie = similarMovies[i];
+
+      // Create HTML elements for each similar movie
+      const similarMovieDiv = document.createElement('div');
+      similarMovieDiv.classList.add('similarMovie');
+      const similarMoviePoster = createMoviePoster(similarMovie.poster_path);
+      similarMoviePoster.setAttribute('id', 'moviePoster');
+      const similarMovieTitle = document.createElement('h2');
+      similarMovieTitle.textContent = similarMovie.title;
+      similarMovieTitle.setAttribute('id', 'movieTitle');
+      const similarMovieOverview = document.createElement('p');
+      similarMovieOverview.setAttribute('id', 'movieOverview')
+      similarMovieOverview.textContent = similarMovie.overview;
+
+      // Append similar movie elements to the container div
+      similarMovieDiv.appendChild(similarMoviePoster);
+      similarMovieDiv.appendChild(similarMovieTitle);
+      similarMovieDiv.appendChild(similarMovieOverview);
+
+      // Append the container div to the similarMoviesDiv
+      similarMoviesDiv.appendChild(similarMovieDiv);
+    }
+  }
 };
